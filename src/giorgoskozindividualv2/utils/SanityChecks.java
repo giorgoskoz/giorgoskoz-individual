07d9192906f4b5e76cafa1e59fc42e2bdac90511
giorgoskoz <giorgoskoz@gmail.com>
2019-01-11 15:42:25 +0200

    created empty class utils.SanityChecks

---
+++ src/giorgoskozindividualv2/utils/SanityChecks.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package giorgoskozindividualv2.utils;
+
+/**
+ *
+ * @author giorgoskoz
+ */
+public class SanityChecks {
+    //if sanityCheck is ok return true, else return false
+    //method overloading??
+}
